aa36234217eb1e2153585bbb00af0e2b75e8f570
supriya sonawane <120164865+supriyadeepaksonawane@users.noreply.github.com>
2022-12-23 03:05:04 -0800

    global

---
+++ global2.js
@@ -0,0 +1,27 @@
+var d=new Date().getDay();
+switch(d)
+{
+    case 0:
+    d ="today its sunday";
+    break;
+    case 1:
+    d ="today its monday";
+    break;
+    case 2 :  
+    d ="today its tuseday";
+    break;
+    case 3 :   
+    d ="today its wensday";
+    break;
+    case 4 : 
+    d ="today its thursday";
+    break;
+    case 5 :  
+    d ="today its friday";
+    break;
+    case 6 :  
+    d ="today its saturday";
+    break;
+
+}
+console.log(d);
